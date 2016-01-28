99bb364f9baa9a27493472abf1d5eca8ea67e797
Yaroslav Rogoza <rogyar@gmail.com>
2016-01-28 16:40:58 +0100

    Fixed extra spaces

--- app/code/Atwix/FirstProduct/Block/Catalog/Product/ListProduct.php
+++ app/code/Atwix/FirstProduct/Block/Catalog/Product/ListProduct.php
@@ -1,35 +1,34 @@
-    <?php
+<?php
 
-    namespace Atwix\FirstProduct\Block\Catalog\Product;
+namespace Atwix\FirstProduct\Block\Catalog\Product;
 
+class ListProduct
+{
+    /**
+     * @var \Magento\Framework\App\Response\Http
+     */
+    protected $response;
 
-    class ListProduct
+    public function __construct(
+       \Magento\Framework\App\Response\Http $response
+    )
     {
-        /**
-         * @var \Magento\Framework\App\Response\Http
-         */
-        protected $response;
-    
-        public function __construct(
-           \Magento\Framework\App\Response\Http $response
-        )
-        {
-            $this->response = $response;
-        }
-
-        /**
-         * @param \Magento\Eav\Model\Entity\Collection\AbstractCollection $resultCollection
-         * @param \Magento\Catalog\Block\Product\ListProduct $subject
-         * @return \Magento\Eav\Model\Entity\Collection\AbstractCollection $resultCollection
-         */
-        public function afterGetLoadedProductCollection(\Magento\Catalog\Block\Product\ListProduct $subject, $resultCollection)
-        {
-            if ($resultCollection->count() == 1) {
-                /** @var \Magento\Catalog\Model\Product $product */
-                $product = $resultCollection->getFirstItem();
-                $this->response->setRedirect($product->getProductUrl());
-            }
-
-            return $resultCollection;
-        }
+        $this->response = $response;
     }
+
+    /**
+     * @param \Magento\Eav\Model\Entity\Collection\AbstractCollection $resultCollection
+     * @param \Magento\Catalog\Block\Product\ListProduct $subject
+     * @return \Magento\Eav\Model\Entity\Collection\AbstractCollection $resultCollection
+     */
+    public function afterGetLoadedProductCollection(\Magento\Catalog\Block\Product\ListProduct $subject, $resultCollection)
+    {
+        if ($resultCollection->count() == 1) {
+            /** @var \Magento\Catalog\Model\Product $product */
+            $product = $resultCollection->getFirstItem();
+            $this->response->setRedirect($product->getProductUrl());
+        }
+
+        return $resultCollection;
+    }
+}
